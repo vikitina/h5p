58cb5592d816b065b01698903811489a4b4fb5d2
Vika <victoria.nikitina@appliedmethods.biz>
2022-02-11 12:22:17 +0200

    config for editor

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,9 @@ module.exports = {
         host: 'localhost',
         proxy: {
           '^/h5p': {
-            target: 'http://127.0.0.1:8080',
-            //  target: 'https://editor-sodix-dev.fwu.de/',
+            //  target: 'http://127.0.0.1:8080',
+              target: 'https://editor-sodix-dev.fwu.de/',
+            // target: 'https://mundo-sodix-dev.fwu.de/',
             changeOrigin: true,
           }
         }
